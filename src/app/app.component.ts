ceb449b32f40bc7451cd3991c7221c2a7cfeaeeb
pgurov <pgurov@ptsecurity.com>
2019-01-30 14:40:57 +0700

    some code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+import { SwUpdate } from '@angular/service-worker';
+
+export interface Book {
+  volumeInfo: {
+    imageLinks: {
+      thumbnail: string;
+    }
+    title: string;
+    authors: string[];
+  };
+}
+
+export interface GBooks {
+  items: Book[];
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'books';
+export class AppComponent implements OnInit {
+  value = '';
+  books = [];
+  newVersionIsAvailable = false;
+  lastQ: string[] = [];
+  loading = false;
+  q = new Subject<string>();
+
+  constructor(private http: HttpClient, private swUpdate: SwUpdate) {
+  }
+
+  reload() {
+    location.reload();
+  }
+
+  ngOnInit() {
+
+    this.swUpdate.available.subscribe(event => {
+      console.log('current version is', event.current);
+      console.log('available version is', event.available);
+      this.newVersionIsAvailable = true;
+    });
+
+    this.q
+      .pipe(
+        map(s => s.trim()),
+        distinctUntilChanged()
+      )
+      .subscribe(s => {
+        if (this.lastQ.length > 5) {
+          this.lastQ.shift();
+        }
+        this.lastQ = [s, ...this.lastQ.filter(lastS => lastS !== s)];
+        this.load(s);
+      });
+  }
+
+  load(s: string) {
+    this.loading = true;
+    this.http.get(`https://www.googleapis.com/books/v1/volumes?q=${s}`)
+      .subscribe((res: GBooks) => {
+        this.books = res.items || [];
+        this.loading = false;
+      });
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { McInputModule, McFormFieldModule, McCardModule, McButtonModule } from '@ptsecurity/mosaic';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    McInputModule,
+    McFormFieldModule,
+    FormsModule,
+    McCardModule,
+    McButtonModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
